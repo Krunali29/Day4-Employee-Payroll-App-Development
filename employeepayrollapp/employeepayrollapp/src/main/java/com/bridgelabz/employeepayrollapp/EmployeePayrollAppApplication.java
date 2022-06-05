f41c5c1a111d8955f371107121162032ebe58eb2
Krunali <krunalilole29@gmail.com>
2022-06-05 18:09:04 +0530

    Adding UC-2-To use @SIf4j at class level,logging is activated

--- employeepayrollapp/employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ employeepayrollapp/employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,18 +12,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome To Employee Payroll Service Application");
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class,args);
+		log.info("Employee Payroll App Started in {} Enivronment",context.getEnvironment().getProperty("environment"));
 	}
-	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.bridgelabz.employeepayrollapp"))
-				.build();
-	}
+//	@Bean
+//	public Docket productApi() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//				.apis(RequestHandlerSelectors.basePackage("com.bridgelabz.employeepayrollapp"))
+//				.build();
+//	}
 }
